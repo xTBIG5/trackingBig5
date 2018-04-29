566bbc86036f67c5a9dd5b03f3ca16e2b808d337
ha <git config --global user.email>
2018-04-29 16:04:39 +0700

    generate login, user-dashboar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { MapService } from './map.service';
+import { LoginComponent } from './login/login.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    LoginComponent,
+    UserDashboardComponent
   ],
   imports: [
     BrowserModule
