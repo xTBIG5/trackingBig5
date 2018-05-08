c0dcac6dfd1d3a3f51487dd6b4218cf26b807c16
ha <git config --global user.email>
2018-05-08 21:20:46 +0700

    add Khoi'work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { MapService } from './map.service';
 import { LoginComponent } from './login/login.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { UserChartComponent } from './user-chart/user-chart.component';
+import { UserChartService } from './user-chart.service';
 
 
 @NgModule({
@@ -14,13 +15,16 @@ import { UserDashboardComponent } from './user-dashboard/user-dashboard.componen
     AppComponent,
     MapComponent,
     LoginComponent,
-    UserDashboardComponent
+    UserDashboardComponent,
+    UserChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [
   	MapService,
+    UserChartService,
 
   ],
   bootstrap: [AppComponent]
